4ce906ea512282eaadc8104ec03af8cd77ee17f8
Niyati Gupta <108604796+Elysian4@users.noreply.github.com>
2024-09-23 00:55:19 +0530

    Update supabase.js

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,7 +1,7 @@
 import {createClient} from "@supabase/supabase-js";
 
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabaseUrl = 'https://ahahhkeitrykikqenssg.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoYWhoa2VpdHJ5a2lrcWVuc3NnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcwMjMzOTUsImV4cCI6MjA0MjU5OTM5NX0.5IFe3WsMDJdLvrvDk2RZfWBucXNSg6-nzsimZnBJfug';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
